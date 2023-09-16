5021556e3e618280c36f2a175ebd558eb2b19fda
Nithiya Streethran <nmstreethran@gmail.com>
2023-09-17 00:02:31 +0100

    convert notebooks to scripts

---
+++ convert-notebooks.sh
@@ -0,0 +1,15 @@
+# convert Jupyter Notebooks to Python scripts
+jupyter nbconvert --to script */*.ipynb
+
+# remove "# In []" and multiple blank lines
+for f in */*.py;
+do sed -i -e '/^# In\[/d' $f
+cat -s $f > $f.txt
+mv $f.txt $f
+done
+
+# format scripts
+black -l 79 */*.py
+
+# move files to scripts directory
+mv notebooks/*.py scripts
