d98c6c52e061aef11036a86130906ba4a8840555
Nithiya Streethran <nmstreethran@gmail.com>
2023-12-30 23:48:26 +0000

    update optimisation

--- src/optimisation.py
+++ src/optimisation.py
@@ -29,7 +29,9 @@ pc = pd.DataFrame(
 # for wind speeds between cut-in and rated
 pc["gradient"] = pc["power_curve"] - pc["power_curve"].shift(1)
 pc["intercept"] = pc["power_curve"] - pc["gradient"] * pc["wind_speed"]
-pc = pc[(pc["wind_speed"] > 4) & (pc["wind_speed"] < 12)]
+pc = pc[
+    (pc["wind_speed"] > REF_V_CUT_IN) & (pc["wind_speed"] < REF_V_RATED + 1)
+]
 
 
 def ref_power_curve(v: float) -> float:
@@ -47,69 +49,69 @@ def ref_power_curve(v: float) -> float:
     - Wind turbine power output at a given wind speed from the power curve [MW]
     """
 
-    if v < 4:
+    if v < REF_V_CUT_IN:
         power_curve = 0
-    elif 4 <= v < 11:
+    elif REF_V_CUT_IN <= v < REF_V_RATED:
         pc_vals = pc[pc["wind_speed"] == np.trunc(v) + 1]
         power_curve = (pc_vals["gradient"] * v + pc_vals["intercept"]).iloc[0]
-    elif 11 <= v <= 25:
+    elif REF_V_RATED <= v <= REF_V_CUT_OUT:
         power_curve = 15000
-    elif v > 25:
+    elif v > REF_V_CUT_OUT:
         power_curve = 0
 
     return power_curve / 1000
 
 
-def rotor_area() -> float:
-    """
-    Reference wind turbine rotor swept area, from Dinh et al. (2023).
-    https://doi.org/10.1016/j.ijhydene.2023.01.016
+# def rotor_area() -> float:
+#     """
+#     Reference wind turbine rotor swept area, from Dinh et al. (2023).
+#     https://doi.org/10.1016/j.ijhydene.2023.01.016
 
-    Returns
-    -------
-    - Wind turbine rotor swept area [m2]
-    """
+#     Returns
+#     -------
+#     - Wind turbine rotor swept area [m2]
+#     """
 
-    return np.pi * np.square(REF_DIAMETER) / 4
-
-
-def power_wind_resource(v: float, rho: float = 1.225) -> float:
-    """
-    Total power of the wind resource passing through the reference wind
-    turbine rotor
-
-    Parameters
-    ----------
-    v : Wind speed [m s-1]
-    rho : Air density [kg m-3]
-
-    Returns
-    -------
-    - Power contained in the wind resource [MW]
-    """
-
-    return 0.5 * rho * rotor_area() * np.power(v, 3) / 1000
+#     return np.pi * np.square(REF_DIAMETER) / 4
 
 
-def power_coefficient(v: float) -> float:
-    """
-    Power coefficient curve of the reference wind turbine
+# def power_wind_resource(v: float, rho: float = 1.225) -> float:
+#     """
+#     Total power of the wind resource passing through the reference wind
+#     turbine rotor
 
-    Parameters
-    ----------
-    v : Wind speed [m s-1]
+#     Parameters
+#     ----------
+#     v : Wind speed [m s-1]
+#     rho : Air density [kg m-3]
 
-    Returns
-    -------
-    - Power coefficient
-    """
+#     Returns
+#     -------
+#     - Power contained in the wind resource [MW]
+#     """
 
-    try:
-        coeff = ref_power_curve(v=v) / power_wind_resource(v=v)
-    except ZeroDivisionError:
-        coeff = 0
+#     return 0.5 * rho * rotor_area() * np.power(v, 3) / 1000
 
-    return coeff
+
+# def power_coefficient(v: float) -> float:
+#     """
+#     Power coefficient curve of the reference wind turbine
+
+#     Parameters
+#     ----------
+#     v : Wind speed [m s-1]
+
+#     Returns
+#     -------
+#     - Power coefficient
+#     """
+
+#     try:
+#         coeff = ref_power_curve(v=v) / power_wind_resource(v=v)
+#     except ZeroDivisionError:
+#         coeff = 0
+
+#     return coeff
 
 
 def weibull_probability_distribution(k: float, c: float, v: float) -> float:
@@ -131,30 +133,52 @@ def weibull_probability_distribution(k: float, c: float, v: float) -> float:
     return k / c * np.power((v / c), (k - 1)) * np.exp(-np.power((v / c), k))
 
 
-def power_output_wind_turbine(v: float) -> float:
-    """
-    Equation (2) of Dinh et al. (2023).
-    https://doi.org/10.1016/j.ijhydene.2023.01.016
+# def power_output_wind_turbine(v: float) -> float:
+#     """
+#     Equation (2) of Dinh et al. (2023).
+#     https://doi.org/10.1016/j.ijhydene.2023.01.016
 
-    Parameters
-    ----------
-    v : Wind speed [m s-1]
+#     Parameters
+#     ----------
+#     v : Wind speed [m s-1]
 
-    Returns
-    -------
-    - Power output of wind turbine [MW]
-    """
+#     Returns
+#     -------
+#     - Power output of wind turbine [MW]
+#     """
 
-    if v < REF_V_CUT_IN:
-        power_wt = 0
-    elif REF_V_CUT_IN <= v < REF_V_RATED:
-        power_wt = power_wind_resource(v=v) * power_coefficient(v=v)
-    elif REF_V_RATED <= v <= REF_V_CUT_OUT:
-        power_wt = REF_RATED_POWER
-    elif v > REF_V_CUT_OUT:
-        power_wt = 0
+#     if v < REF_V_CUT_IN:
+#         power_wt = 0
+#     elif REF_V_CUT_IN <= v < REF_V_RATED:
+#         power_wt = power_wind_resource(v=v) * power_coefficient(v=v)
+#     elif REF_V_RATED <= v <= REF_V_CUT_OUT:
+#         power_wt = REF_RATED_POWER
+#     elif v > REF_V_CUT_OUT:
+#         power_wt = 0
 
-    return power_wt
+#     return power_wt
+
+
+# def power_curve_weibull(k: float, c: float, v: float) -> float:
+#     """
+#     Power curve with Weibull equation applied
+
+#     Parameters
+#     ----------
+#     k : Shape (Weibull distribution parameter)
+#     c : Scale (Weibull distribution parameter) [m s-1]
+#     v : Wind speed [m s-1]
+
+#     Returns
+#     -------
+#     - Power curve multiplied by the Weibull probability distribution function
+#       [MW s m-1 / M kg m s-2]
+#     """
+
+#     return (
+#         ref_power_curve(v=v) *
+#         weibull_probability_distribution(k=k, c=c, v=v)
+#     )
 
 
 def annual_energy_production(
@@ -167,6 +191,9 @@ def annual_energy_production(
     Equation (3) of Dinh et al. (2023).
     https://doi.org/10.1016/j.ijhydene.2023.01.016
 
+    In the integration, both the limit and absolute error tolerance have
+    been increased.
+
     Parameters
     ----------
     n_turbines : Number of wind turbines in wind farm
@@ -176,26 +203,23 @@ def annual_energy_production(
 
     Returns
     -------
-    - Annual energy production of wind farm [MWh]
+    - Annual energy production of wind farm [GWh]
     """
 
-    aep = (
-        365
-        * 24
-        * n_turbines
-        * (1 - w_loss)
-        * integrate.quad(
-            lambda v: (
-                # power_output_wind_turbine(v=v)
-                ref_power_curve(v=v)
-                * weibull_probability_distribution(k=k, c=c, v=v)
-            ),
-            REF_V_CUT_IN,
-            REF_V_CUT_OUT,
-        )
+    integration = integrate.quad(
+        lambda v: (
+            ref_power_curve(v=v)
+            * weibull_probability_distribution(k=k, c=c, v=v)
+        ),
+        a=REF_V_CUT_IN,
+        b=REF_V_CUT_OUT,
+        limit=100,  # an upper bound on the number of subintervals used
+        epsabs=1.49e-6  # absolute error tolerance
     )
 
-    return aep
+    aep = 365 * 24 * n_turbines * (1 - w_loss) * integration[0]
+
+    return aep / 1000, integration
 
 
 def annual_hydrogen_production(
