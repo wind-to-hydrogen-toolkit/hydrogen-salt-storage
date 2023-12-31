7826293737da4eec5c4d0db030ebb3ff4faaec0e
Nithiya Streethran <nmstreethran@gmail.com>
2023-12-31 22:45:42 +0000

    use pydata-sphinx-theme

--- docs/conf.py
+++ docs/conf.py
@@ -24,5 +24,21 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    'icon_links': [
+        {
+            # Label for this link
+            'name': 'GitHub',
+            # URL where the link will redirect
+            'url': 'https://github.com/nmstreethran/hydrogen-salt-storage-optimisation',  # required
+            # Icon class (if 'type': 'fontawesome'), or path to local image (if 'type': 'local')
+            'icon': 'fa-brands fa-github',
+            # The type of image to be used (see below for details)
+            'type': 'fontawesome',
+        }
+    ],
+    'navbar_align': 'right'
+}
