59a0dccc54ad34affac25155d940260893efe810
Nithiya Streethran <nmstreethran@gmail.com>
2024-01-05 19:28:51 +0000

    update convert-notebooks.sh

--- convert-notebooks.sh
+++ convert-notebooks.sh
@@ -4,10 +4,13 @@
 source .venv/bin/activate
 
 # convert Jupyter Notebooks to Python scripts
-jupyter nbconvert --to script */*.ipynb
+jupyter nbconvert --to script docs/notebooks/*.ipynb
+
+# move scripts to scripts/ directory
+mv docs/notebooks/*.py scripts
 
 # remove "# In []" and multiple blank lines in converted scripts
-for f in temp/*.py;
+for f in scripts/*.py;
 do sed -i -e '/^# In\[/d' $f
 cat -s $f > $f.txt
 mv $f.txt $f
